25e29b1c6bdf3d98facff22175a8cb3cc10438d4
Toby <tobykitchen@gmail.com>
2023-04-17 20:32:27 +1000

    usertest initial commit

---
+++ src/userTest.java
@@ -0,0 +1,14 @@
+public class userTest {
+    /**
+    We need a class for a quiz, that contains all the required data (e.g. questions, answers,
+     and a bunch of metadata for searching such as difficulty rating, target audience, etc);
+     then a class to handle delivering the quiz to the user
+     (I think similar to the executive function assessment interface Nick made).
+
+    I'm also thinking that we'll need a method to call the quizzes from storage
+     (i.e. convert from csv (?) or however they're stored, into an instance of the quiz class,
+     and then into some kind of array for searching maybe?).
+     I'm not sure where that would go but perhaps that's something to consider
+     once the rest of the module is designed.
+     **/
+}
