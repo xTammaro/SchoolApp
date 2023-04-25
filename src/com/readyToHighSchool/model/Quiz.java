66ac60e0e8e347406579724e259a46cea1100fc5
Jake <u7074622@anu.edu.au>
2023-04-25 15:00:37 +1000

    Added images to quiz.

--- src/com/readyToHighSchool/model/Quiz.java
+++ src/com/readyToHighSchool/model/Quiz.java
@@ -13,13 +13,23 @@ public class Quiz {
     private LinkedList<Question> questions; // Enables student to go skip ahead or go back
     private int executiveFunctionScore;
     private boolean completed;
+    private List<String> imageUrls
     //private int timeLimit; Not sure if these will be timed quizzes.
 
-    public Quiz(String title, LinkedList<Question> questions, int executiveFunctionScore) {
+    public Quiz(String title, LinkedList<Question> questions, int executiveFunctionScore, List<String> imageUrls) {
         this.id = id;
         this.title = title;
         this.questions = questions;
         this.executiveFunctionScore = executiveFunctionScore;
+        this.imageUrls = imageUrls;
+    }
+
+    public List<String> getImageUrls() {
+        return imageUrls;
+    }
+
+    public void setImageUrls(List<String> imageUrls) {
+        this.imageUrls = imageUrls;
     }
 
     public String getId() {
